d608d6847c5adee9c2b56db8cc4644226e597e08
abhinav0306 <abhinavs0603@gmail.com>
2023-11-06 15:25:50 +0530

    numeron1

--- game.js
+++ game.js
@@ -1,5 +1,70 @@
+const greater = document.getElementById("greater-than")
+const equal = document.getElementById("equal-to")
+const lesser = document.getElementById("lesser-than")
+const timer = document.getElementById("timer")
 // Iteration 2: Generate 2 random number and display it on the screen
-
+function generateRandom(){
+    num1 = Math.round(Math.random()*100)
+    num2 = Math.round(Math.random()*100)
+    
+    number1.innerText = num1
+    number2.innerText = num2
+}
+generateRandom();
+startTimer();
 // Iteration 3: Make the options button functional
-
+let score =0;
+greater.onclick = ()=>{
+    if(num1<=num2){
+        location.href = 'gameover.html'
+    }
+    else{
+        score++
+        resetTimer(timerID)
+        // localStorage.clear
+        // localStorage.setItem("score",score);
+        generateRandom();
+    } 
+}
+equal.onclick = ()=>{
+    if(num1 != num2){
+        location.href = 'gameover.html'
+    } else{
+        score++
+        resetTimer(timerID)
+        // localStorage.clear
+        // localStorage.setItem("score",score);
+        generateRandom();
+    }XMLDocument
+}
+lesser.onclick = ()=>{
+    if(num1>=num2){
+        location.href = 'gameover.html'
+    } else{
+        score++
+        // localStorage.clear
+        // localStorage.setItem("score",score);
+        resetTimer(timerID)
+        generateRandom();
+    }
+}
 // Iteration 4: Build a timer for the game
+var timerID;
+function startTimer(){
+    var time = 5;
+    timer.innerText = time;
+    timerID = setInterval(()=>{
+        time--;
+        localStorage.setItem("score",score)
+        if(time ==0){
+            location.href = 'gameover.html'
+        }
+        else{
+            timer.innerText = time
+        }
+    },1000)
+}
+function resetTimer(intervalid){
+    clearInterval(intervalid)
+    startTimer();
+}
--- score.js
+++ score.js
@@ -1,1 +1,10 @@
 // Iteration 5: Store the player score and display it on the game over screen
+const display_score = document.getElementById("score-board")
+const plbtn = document.getElementById("play-again-button")
+
+let score = localStorage.getItem("score")
+display_score.innerText = score;
+
+plbtn .onclick=()=>{
+    location.href = 'game.html'
+}
